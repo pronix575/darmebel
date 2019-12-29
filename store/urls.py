3112d15d16aa827381a897b712df35b12423a20e
pronix <proenix575@gmail.com>
2019-12-29 13:57:28 +0300

    back is done

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views
+from .views import CategoryList
 
 urlpatterns = [
-    path('', views.work_list, name='work_list'),
+    path('', views.main_list, name='main_list'),
+    path('categories', CategoryList.as_view())
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,18 @@
+from django.views.generic import ListView
+from .models import Work, Category
 from django.shortcuts import render
-from .models import Work
 from django.utils import timezone
 
-def work_list(request):
+def main_list(request):
     works = Work.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'store/work_list.html', {'works': works})
+    categories = Category.objects.all()
+
+    return render(request, 'store/main_list.html', {
+        'works': works, 'categories': categories
+    })
+
+
+class CategoryList(ListView):
+    queryset = Category.objects.all()
+    context_object_name = 'category_list'
+    template_name = 'store/category_list.html'
