1e71c870069272732ca7c9c711a6ce64195546de
pronix <proenix575@gmail.com>
2020-01-15 01:53:47 +0300

    slider return

--- store/static/store/js/search.js
+++ store/static/store/js/search.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+	var $pront = $(".price")
 	
-   
+	var 	price       = $pront.html(),
+            price_sep   = price.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");	   
+
+            $pr.empty()
+
+            $pr.html(price_sep + " ₽")
 
 });
