6a8f5316bae3d6752d13ec986123bf932db82a52
pronix <proenix575@gmail.com>
2020-01-25 22:57:30 +0300

    form

--- store/models.py
+++ store/models.py
@@ -34,3 +34,8 @@ class Work(models.Model):
 
     def __str__(self):
         return self.name
+
+class Request(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    
